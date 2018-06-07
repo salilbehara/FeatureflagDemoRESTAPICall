aa3d9765e6d597f441f7694aedf8e23fca02a85e
Salil Behara <SBehara@ebsco.com>
2018-06-07 14:10:23 -0500

    Feature Flag Demo - REST API call

---
+++ FeatureflagDemo/FeatureFlag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FeatureflagDemo
+{
+    public class FeatureSettings
+    {
+        public string name { get; set; }
+        public bool qa { get; set; }
+        public bool prod { get; set; }
+        public string application { get; set; }
+        public bool dev { get; set; }
+    }
+
+    public class Item
+    {
+        public FeatureSettings FeatureSettings { get; set; }
+        public string appName { get; set; }
+        public string Feature_ID { get; set; }
+    }
+
+    public class AppFeatureFlagData
+    {
+        public List<Item> Items { get; set; }
+        public int Count { get; set; }
+        public int ScannedCount { get; set; }
+    }
+
+    public class FeatureFlagResult
+    {
+        public Item Item { get; set; }
+    }
+
+}
--- FeatureflagDemo/Program.cs
+++ FeatureflagDemo/Program.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace FeatureflagDemo
 {
-    class Program
+    public class Program
     {
+        private const string urlAppData = "https://lrplv54dni.execute-api.us-east-1.amazonaws.com/FFTEST/flagdata/appdata ";
+        private const string urlAppDataParameters = "?appName=Customer Authority";
+
+        private const string urlFlagData = "https://lrplv54dni.execute-api.us-east-1.amazonaws.com/FFTEST/flagdata";
+        private static string urlFlagDataParameters = "?featureName=02 test_feature";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            var appFeatureFlags = GetAppFeatureFlags().Result;
+            foreach (var item in appFeatureFlags.Items)
+            {
+                DisplayFeatureFlag(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Enter a Feature Flag to search:");
+            urlFlagDataParameters = $"?featureName={Console.ReadLine()}";
+
+            var featureFlagResponse = GetFeatureFlag(urlFlagDataParameters).Result;
+            Console.WriteLine();
+            DisplayFeatureFlag(featureFlagResponse.Item);
+
+            Console.ReadLine();
+        }
+
+        private static async Task<AppFeatureFlagData> GetAppFeatureFlags()
+        {
+            using (var client = new HttpClient())
+            {
+                AppFeatureFlagData featureFlags = null;
+
+                client.BaseAddress = new Uri(urlAppData);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(urlAppDataParameters);
+                if (response.IsSuccessStatusCode)
+                {
+                    featureFlags = await response.Content.ReadAsAsync<AppFeatureFlagData>();
+                    return featureFlags;
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                    return featureFlags;
+                }
+            }
+        }
+
+        private static async Task<FeatureFlagResult> GetFeatureFlag(string featureFlagSearchParm)
+        {
+            using (var client = new HttpClient())
+            {
+                FeatureFlagResult featureFlags = null;
+
+                client.BaseAddress = new Uri(urlFlagData);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(featureFlagSearchParm);
+                if (response.IsSuccessStatusCode)
+                {
+                    featureFlags = await response.Content.ReadAsAsync<FeatureFlagResult>();
+                    return featureFlags;
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                    return featureFlags;
+                }
+            }
+        }
+
+        static void DisplayFeatureFlag(Item item)
+        {
+            Console.WriteLine($"Application Name    --> {item.FeatureSettings.application}");
+            Console.WriteLine($"Feature Name        --> {item.FeatureSettings.name}");
+            Console.WriteLine($"DEV Flag            --> {item.FeatureSettings.dev}");
+            Console.WriteLine($"QA Flag             --> {item.FeatureSettings.qa}");
+            Console.WriteLine($"PROD Flag           --> {item.FeatureSettings.prod}");
+
+            Console.WriteLine();
         }
     }
 }
